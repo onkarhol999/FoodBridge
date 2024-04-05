60d99c0fb91e6dbe022e9021f70b2bd7c1ee9029
JayPalkar <jaypalkar654@gmail.com>
2024-04-05 15:46:37 +0530

    Navbar completed

--- Client/src/components/navbar/Navbar.jsx
+++ Client/src/components/navbar/Navbar.jsx
@@ -1,44 +1,39 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
+import '../navbar/Navbar.css';
+import logo1 from '../images/FoodBridge.png'
 
 function Navbar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider"/></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
+      <div>
+        <nav className="navbar navbar-expand-lg ">
+          <div className="container-fluid">
+              <a className="navbar-brand" href="#">
+                  <img className='navLogo mx-2' src={logo1}/>
+                  <p>FoodBridge </p>
+              </a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul className="navbar-nav  ms-auto order-5 mx-5 nav-underline">
+                      <li className="nav-item mx-3">
+                      <a className="nav-link" aria-current="page" href="#">How To Help</a>
+                      </li>
+                      <li className="nav-item mx-3">
+                      <a className="nav-link" href="#">What we do?</a>
+                      </li>
+                      <li className="nav-item mx-3">
+                      <a className="nav-link" href="#">Contact Us</a>
+                      </li>
+                      <li className="nav-item mx-3">
+                      <a className="nav-link" href="#">Admin Login</a>
+                      </li>
+                  </ul>
+              </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </>
   )
 }
