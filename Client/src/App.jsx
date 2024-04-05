ff18b77ff4b39b95dd3eb1e1fd1080f3fe761b0e
JayPalkar <jaypalkar654@gmail.com>
2024-04-05 20:14:14 +0530

    Homepage completed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'
-import Navbar from './components/navbar/Navbar'
-// import DonationForm from './components/DonationUserData/DonationForm.jsx';
-import HomePage from './components/HomePage/homePage.jsx';
-import Footer from './components/Footer/Footer.jsx';
+// import './App.css'
+// import Navbar from './components/navbar/Navbar'
+// // import DonationForm from './components/DonationUserData/DonationForm.jsx';
+// import HomePage from './components/HomePage/homePage.jsx';
+// // import DiliveryDashboard from './components/DeliveryDashboard/DiliveryDashboard.jsx'
+// import Footer from './components/Footer/Footer.jsx';
 
 
-function App() {
-  return (
-    <>
-      <Navbar/>
-      <HomePage/>
-      {/* <DonationForm/> */}
-      <Footer/>
-    </>
-  )
-}
+// function App() {
+//   return (
+//     <>
+//       <Navbar/>
+//       <HomePage/>
+//       {/* <DonationForm/> */}
+//       {/* <DiliveryDashboard/> */}
+//       <Footer/>
+//     </>
+//   )
+// }
 
-export default App
+// export default App
